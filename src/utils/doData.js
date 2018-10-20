5de709b35e234a53b32fea129a61f906ff74aeda
yhx9527 <540230904@qq.com>
2018-10-20 22:26:54 +0800

    改改改

--- src/utils/doData.js
+++ src/utils/doData.js
@@ -15,13 +15,48 @@ export default class {
       return value1 - value2
     }
   }
+ // 时间格式化
+  timeFormat(timeString) {
+    let date = new Date(Date.parse(timeString))
+    let month = date.getMonth() + 1
+    let hours = date.getHours() < 10 ? ('0' + date.getHours()) : date.getHours()
+    let minutes = date.getMinutes() < 10 ? ('0' + date.getMinutes()) : date.getMinutes()
+    return date.getFullYear() + '年' + month + '月' + date.getDate() + '日' + '  ' + hours + '时' + minutes + '分'
+  }
+  timeFormat1(timeString) {
+    let date = new Date(Date.parse(timeString))
+    let time = timeString.split('T')[1]
+    let month = date.getMonth() + 1
+    return date.getFullYear() + '年' + month + '月' + date.getDate() + '日' + '  ' + time
+  }
+  // 当前时间比较
+  compareTime(kstime) {
+    let temp = Date.parse(kstime)
+    let now = Date.now()
+    if (now > temp) {
+      return true
+    } else if (now < temp) {
+      return false
+    }
+  }
+  oneSubject(item) {
+    var that = this
+    item.ifPast = that.compareTime(item.ksStartTime)
+    item.ifEnd = that.compareTime(item.ksEndTime)
+    item.ksEndTime = that.timeFormat(item.ksEndTime)
+    item.ksStartTime = that.timeFormat(item.ksStartTime)
+    return item
+  }
   subject(list) {
+    var that = this
     if (list === null) {
       return []
     }
     return list.filter(item => {
-      item.ksEndTime = item.ksEndTime.split('T').join(' ')
-      item.ksStartTime = item.ksStartTime.split('T').join(' ')
+      item.ifPast = that.compareTime(item.ksStartTime)
+      item.ifEnd = that.compareTime(item.ksEndTime)
+      item.ksEndTime = that.timeFormat(item.ksEndTime)
+      item.ksStartTime = that.timeFormat(item.ksStartTime)
       return item.ksConfirm === true
     })
   }
@@ -34,10 +69,13 @@ export default class {
     })
   }
   subInfo(subject, kuId) {
+    var that = this
     subject.enroll = 0
     subject.park = 0
-    subject.ksEndTime = subject.ksEndTime.split('T').join(' ')
-    subject.ksStartTime = subject.ksStartTime.split('T').join(' ')
+    subject.ifPast = that.compareTime(subject.ksStartTime)
+    subject.ifEnd = that.compareTime(subject.ksEndTime)
+    subject.ksEndTime = that.timeFormat(subject.ksEndTime)
+    subject.ksStartTime = that.timeFormat(subject.ksStartTime)
     subject.ksEnrollList.forEach(item => {
       if (item.kuId === kuId) {
         subject.enroll = item.keStatus ? 2 : 1
